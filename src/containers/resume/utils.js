aee68ab03247eddcdd26e00de1a637a357462736
zahraHasanzada <116168664+zahraHasanzada@users.noreply.github.com>
2023-12-04 08:26:15 -0600

    Add files via upload

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,9 @@
+export const data ={
+    education : [
+        {
+            title : "Front End Developer",
+            subTitle : "CTI, Herat-Af"
+            description :"CTI is a "
+        }
+    ]
+}
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,57 @@
+ export  const SkillsData = [
+  {
+    label: "FRONT END",
+    data: [
+      {
+        skillName: "HTML",
+        percentage: "95",
+      },
+      {
+        skillName: "CSS",
+        percentage: "70",
+      },
+      {
+        skillName: "JAVASCRIPT",
+        percentage: "80",
+      },
+    
+    ],
+  },
+  {
+    label: "BACK END",
+    data: [
+      {
+        skillName: "JAVA",
+        percentage: "90",
+      },
+      {
+        skillName: "PYTHON",
+        percentage: "50",
+      },
+      {
+        skillName: "NODE JS",
+        percentage: "50",
+      },
+    ],
+  },
+
+{
+  label: "LANGUAGE",
+  data: [
+    {
+      skillName: "PERSIAN",
+      percentage: "100",
+    },
+    {
+      skillName: "ENGLISH",
+      percentage: "50",
+    },
+    {
+      skillName: "PASHTO",
+      percentage: "50",
+    },
+  ],
+},
+
+  ]
+  
